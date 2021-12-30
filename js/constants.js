60e69e7c49a04c4ba644b4566a5f252b522870ac
Cesar G. Gatica Rosales <90020224+cesargatica@users.noreply.github.com>
2021-12-29 18:59:26 -0600

    cambio en constants

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 export const API_KEY = 'f235af951a6a4c418b00273a449f9ad5'
-export const BASE_API = 'http://api.openweathermap.org/data/2.5/'
+export const BASE_API = 'https://api.openweathermap.org/data/2.5/'
 
 export const weatherConditionsCodes = {
     2: 'rainy',
